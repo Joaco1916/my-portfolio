6d8c37fede810156d46b32fa2199694173b7b7b0
Joaco1916 <31074929+Joaco1916@users.noreply.github.com>
2023-02-14 00:08:27 -0300

    Color Palette - Stormi

---
+++ src/utils/colorPalette.js
@@ -0,0 +1,9 @@
+export const colorPalette = {
+    stormi: {
+        darkest:'#3F7396',
+        darker:'#4F8EB5',
+        normal:'#77BDDF',
+        lighter:'#A4E2F9',
+        lightest:'#D6F5FF'
+    }
+}
