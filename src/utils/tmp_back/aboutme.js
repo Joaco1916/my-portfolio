504f2adde8a21e758315da90f0e7cc26ece10330
Joaco1916 <31074929+Joaco1916@users.noreply.github.com>
2023-02-14 03:31:15 -0300

    Static data - about me and education

---
+++ src/utils/tmp_back/aboutme.js
@@ -0,0 +1,20 @@
+export const aboutme = {
+    es:{
+        title: 'Sobre mí',
+        descriptions: [
+            'Hola, mi nombre es Joaquín. Soy un desarrollador que le gusta estar en constante aprendizaje de nuevas herramientas y recursos. El aprendizaje siempre acompañado de un buen mate.',
+            'Actualmente estoy transcurriendo mi estapa final de la carrera de Ingeniería en Sistemas de Información en la Universidad Nacional del Sur. Me encuentro preparando el proyecto final para recibirme.',
+            'Mi experiencia es principalmente en el desarrollo de aplicaciones web, pero también he trabajado en aplicaciones móviles y de escritorio.',
+            'En mi tiempo libre me gusta dibujar, leer ó escuchar música, si es posible, al aire libre y en el cesped.'
+        ],
+    },
+    en:{
+        title: 'About me',
+        descriptions: [
+            'Hi, my name is Joaquín. I am a developer who likes to be constantly learning new tools and resources. This learning always accompanied by a good mate.',
+            'I am currently in my final stage of the Software Engineer career at the Universidad Nacional del Sur. I am preparing the final project to graduate.',
+            'My experience is mainly in web application development, but I have also worked on mobile and desktop applications.',
+            'In my free time I like to draw, read or listen to music, if possible, outdoors and on the grass.'
+        ],
+    }
+}
---
+++ src/utils/tmp_back/studies.js
@@ -0,0 +1,98 @@
+export const studies = [
+    {
+        id: 1,
+        title: {
+            es:'Ingeniería en Sistemas de Información',
+            en:'Software Engineer',
+        },
+        institute: 'Universidad Nacional del Sur',
+        location: 'Bahía Blanca, Buenos Aires, Argentina',
+        metodology: {
+            es:'Presencial',
+            en:'Presencial',
+        },
+        icon: {
+            path: '/studies/uns-logo.png',
+        },
+        startDate: {
+            es: 'Marzo 2014',
+            en: 'March 2014',
+        },
+        endDate:{
+            es: 'Actualidad',
+            en: 'Present',
+        }
+    },
+    {
+        id: 2,
+        title: {
+            es:'Introducción a Kotlin',
+            en:'Introduction to Kotlin',
+        },
+        institute: 'Universidad Nacional del Sur',
+        location: 'Bahía Blanca, Buenos Aires, Argentina',
+        metodology: {
+            es:'Presencial',
+            en:'Presencial',
+        },
+        icon: {
+            path: '/studies/eati.png',
+        },
+        startDate: {
+            es: 'Enero 2019',
+            en: 'January 2019',
+        },
+        endDate:{
+            es: 'Febrero 2019',
+            en: 'February 2019',
+        }
+    },
+    {
+        id: 3,
+        title: {
+            es:'First Certificate Exam',
+            en:'First Certificate Exam',
+        },
+        institute: 'Cambridge English',
+        location: 'Bahía Blanca, Buenos Aires, Argentina',
+        metodology: {
+            es:'Presencial',
+            en:'Presencial',
+        },
+        icon: {
+            path: '/studies/cambridge.svg',
+        },
+        startDate: {
+            es: 'Abril 2017',
+            en: 'April 2017',
+        },
+        endDate:{
+            es: 'Diciembre 2017',
+            en: 'December 2017',
+        }
+    },
+    {
+        id: 4,
+        title: {
+            es:'Bachillerato en Ciencia Naturales',
+            en:'Bachillerato en Ciencia Naturales',
+        },
+        institute: 'Instituto Cannossiano Sagrado Corazón',
+        location: 'Punta Alta, Buenos Aires, Argentina',
+        metodology: {
+            es:'Presencial',
+            en:'Presencial',
+        },
+        icon: {
+            path: '/studies/cannossiano.jpg',
+        },
+        startDate: {
+            es: 'Marzo 2008',
+            en: 'March 2008',
+        },
+        endDate:{
+            es: 'Diciembre 2013',
+            en: 'December 2013',
+        }
+    },
+]
